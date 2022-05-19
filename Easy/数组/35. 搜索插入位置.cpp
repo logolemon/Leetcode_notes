c448662053765190a4b48e4c0a318a44c441f6b9
Logo、 <101202988+logolemon@users.noreply.github.com>
2022-05-19 16:38:09 +0800

    Update 35. 搜索插入位置.cpp

--- Easy/数组/35. 搜索插入位置.cpp
+++ Easy/数组/35. 搜索插入位置.cpp
@@ -29,6 +29,16 @@ public:
 };
 
 //第二种：采用STL容器的解法
+//lower_bound()函数用于在指定的区域内查找不小于目标值的第一个元素，简单的说，就是查找的比目标值大的第一个元素
+//lower_bound() 函数定义在<algorithm>头文件中，其语法格式有 2 种，分别为：
+//在 [first, last) 区域内查找不小于 val 的元素
+ForwardIterator lower_bound (ForwardIterator first, ForwardIterator last,
+                             const T& val);
+//在 [first, last) 区域内查找第一个不符合 comp 规则的元素
+ForwardIterator lower_bound (ForwardIterator first, ForwardIterator last,
+                             const T& val, Compare comp);
+//first，last分别是容器的起始迭代器，这个函数最后返回的也是一个迭代器，val是目标函数，就是这个题的target，然后最后一个参数comp是自定义规则，就是可以自己制定它的规则。
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
