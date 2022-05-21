050d8ed4895e028ccd1f156a89495a0957e6c97a
Logo、 <101202988+logolemon@users.noreply.github.com>
2022-05-21 22:34:42 +0800

    Update 动态规划.cpp

--- 算法笔记/动态规划.cpp
+++ 算法笔记/动态规划.cpp
@@ -222,6 +222,37 @@ public:
 // dp[i] = min(dp[i+1][j]，dp[i+1][j+1]) + triangle[i][j];
 // 3、返回dp[0][0]
 
+// 解法1：
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        if(triangle.size() == 0){
+            return 0;
+        }
+
+        std::vector<std::vector<int>> dp;
+
+        for(int i = 0; i < triangle.size(); i++){
+            dp.push_back(std::vector<int>());
+            for(int j = 0; j < triangle.size(); j++){
+                dp[i].push_back(0);
+            }
+        }
+
+        for(int i = 0; i < dp.size(); i++){
+            dp[dp.size() - 1][i] = triangle[dp.size() - 1][i]; 
+        }
+
+        for(int i = dp.size() - 2; i >= 0; i--){//从倒数第二层开始
+            for(int j = 0; j < dp[i].size(); j++)
+                dp[i][j] = std::min(dp[i+1][j], dp[i+1][j+1]) + triangle[i][j];
+        }
+        return dp[0][0];
+        
+
+    }
+};
 
 
 
