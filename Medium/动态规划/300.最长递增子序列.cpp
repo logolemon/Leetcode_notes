b2a4da6d313ff64e6b685736d0705886b5a93dd4
Logo、 <101202988+logolemon@users.noreply.github.com>
2022-05-25 21:36:20 +0800

    Update 300.最长递增子序列.cpp

--- Medium/动态规划/300.最长递增子序列.cpp
+++ Medium/动态规划/300.最长递增子序列.cpp
@@ -30,6 +30,7 @@ public:
     }
 };
 
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // 解法2：
 // nums = [1, 3,2,3,1,4]
 // 1.设置一个栈(使用vector实现)，将nums[0] push栈中。
@@ -60,6 +61,56 @@ public:
                         break;
                     }
                 }
+            }
+        }
+        return stack.size();
+    }
+};
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// 解法2优化(n*logn)：
+// 二分查找nums[i]的位置
+int binary_search(std::vector<int> nums, int target){
+    int index = -1;
+    int begin = 0;
+    int end = nums.size() - 1;
+    while(index == -1){
+        int mid = (begin + end) / 2;
+        if(target == nums[mid]){
+            index = mid;
+        }
+        else if(target < nums[mid]){
+            if(mid == 0 || target > nums[mid - 1]){
+                index = mid;
+            }
+            end = mid - 1;
+        }
+        else if(target > nums[mid]){
+            if(mid == nums.size() - 1 || target < nums[mid + 1]){
+                index = mid + 1;
+            }
+            begin = mid + 1;
+        }
+    }
+    return index;
+}
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size() == 0){
+            return 0;
+        }//如果数组为空，则返回0
+
+        std::vector<int> stack;//设置一个vector
+        stack.push_back(nums[0]);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] > stack.back()){
+                stack.push_back(nums[i]);
+            }
+            else{
+                int pos = binary_search(stack, nums[i]);
+                stack[pos] = nums[i];
             }
         }
         return stack.size();
