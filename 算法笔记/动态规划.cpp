dd7fcc9e2fbc52590af7775fa096bc2765af9394
Logo、 <101202988+logolemon@users.noreply.github.com>
2022-05-25 21:38:28 +0800

    Update 动态规划.cpp

--- 算法笔记/动态规划.cpp
+++ 算法笔记/动态规划.cpp
@@ -354,7 +354,53 @@ public:
     }
 };
 
+// 上面的解法优化：
+int binary_search(std::vector<int> nums, int target){
+    int index = -1;
+    int begin = 0;
+    int end = nums.size() - 1;
+    while(index == -1){
+        int mid = (begin + end) / 2;
+        if(target == nums[mid]){
+            index = mid;
+        }
+        else if(target < nums[mid]){
+            if(mid == 0 || target > nums[mid - 1]){
+                index = mid;
+            }
+            end = mid - 1;
+        }
+        else if(target > nums[mid]){
+            if(mid == nums.size() - 1 || target < nums[mid + 1]){
+                index = mid + 1;
+            }
+            begin = mid + 1;
+        }
+    }
+    return index;
+}
 
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size() == 0){
+            return 0;
+        }//如果数组为空，则返回0
+
+        std::vector<int> stack;//设置一个vector
+        stack.push_back(nums[0]);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] > stack.back()){
+                stack.push_back(nums[i]);
+            }
+            else{
+                int pos = binary_search(stack, nums[i]);
+                stack[pos] = nums[i];
+            }
+        }
+        return stack.size();
+    }
+};
 
 
 
