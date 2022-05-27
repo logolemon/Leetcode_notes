0343ca2d57a0a19ac233b6804ecf4ce6291e906f
Logo、 <101202988+logolemon@users.noreply.github.com>
2022-05-27 13:44:38 +0800

    Update 283.移动零.cpp

--- Easy/数组/283.移动零.cpp
+++ Easy/数组/283.移动零.cpp
@@ -22,3 +22,21 @@ public:
         }
     }
 };
+
+// 解法2：双指针思路
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int n = nums.size(), left = 0, right = 0;
+
+        while(right < n){
+            if(nums[right]){
+                swap(nums[left], nums[right]);
+                left++;
+            }
+            right++;
+        }
+
+    }
+};
