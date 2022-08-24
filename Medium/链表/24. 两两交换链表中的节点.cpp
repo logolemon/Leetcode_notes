513e8bce29c65370180ca44a1e1e2675ef7b2efa
Logo、 <101202988+logolemon@users.noreply.github.com>
2022-08-24 15:18:34 +0800

    Update 24. 两两交换链表中的节点.cpp

--- Medium/链表/24. 两两交换链表中的节点.cpp
+++ Medium/链表/24. 两两交换链表中的节点.cpp
@@ -25,3 +25,20 @@ public:
 };//执行用时：4ms 内存消耗：7.3MB
 
 //递归解法：
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        //终止条件：链表只剩一个节点或者没节点了，没得交换了。返回的是已经处理好的链表
+        if(head == nullptr || head->next == nullptr){
+            return head;
+        }
+        //一共三个节点：head、head->next、swapPairs(next->next)
+        //下面的任务便是交换这3个节点的前面两个节点
+        ListNode *next = head->next;
+        head->next = swapPairs(next->next);
+        next->next = head;
+        //根据第二步，返回给上一级的是当前已经完成交换后，即处理好了的链表部分
+        return next;
+           
+    }
+};//执行用时：0ms 内存消耗：7.3MB
