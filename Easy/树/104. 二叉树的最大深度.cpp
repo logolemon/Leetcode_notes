ce53e76c774d1adf41d5fa8746d16afb6a762915
Logo、 <101202988+logolemon@users.noreply.github.com>
2022-08-24 14:43:00 +0800

    Create 104. 二叉树的最大深度.cpp

---
+++ Easy/树/104. 二叉树的最大深度.cpp
@@ -0,0 +1,10 @@
+//给定一个二叉树，找出其最大深度。二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。说明: 叶子节点是指没有子节点的节点。
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+
+    }
+};//执行用时：0ms 内存消耗：18.4MB
